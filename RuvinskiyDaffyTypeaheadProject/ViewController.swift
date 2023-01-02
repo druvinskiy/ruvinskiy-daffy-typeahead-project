caa1b3024b07ce3c9412e47f3b7b16c876ba0d51
David Ruvinskiy <david.ruvinskiy@sbcglobal.net>
2023-01-02 13:22:30 -0600

    Initial commit

---
+++ RuvinskiyDaffyTypeaheadProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RuvinskiyDaffyTypeaheadProject
+//
+//  Created by David Ruvinskiy on 1/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
